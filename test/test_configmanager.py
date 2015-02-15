38e9d522910a53e31846cd7b33a8d2c441a807cc
Boris Rybalkin <ribalkin@gmail.com>
2015-02-15 11:27:15 +0000

    unittest -> py.test

--- test/test_configmanager.py
+++ test/test_configmanager.py
@@ -1,8 +1,6 @@
-import unittest
 from os.path import dirname
 
 from syncloud.owncloud.configmanager import ConfigManager
-
 
 test_dir = dirname(__file__)
 temp_config_file = test_dir + '/data/config.php'
@@ -12,59 +10,64 @@ def read_file(test_file):
         return open(test_dir + '/data/' + test_file).read()
 
 
-class TestConfigManager(unittest.TestCase):
+def test_trusted_config():
 
-    def test_trusted_config(self):
+    config = ConfigManager(temp_config_file)
+    config.write(read_file('config_trusted_domains_input.php'))
+    config.trusted('test.domain', 1000)
 
-        config = ConfigManager(temp_config_file)
-        config.write(read_file('config_trusted_domains_input.php'))
-        config.trusted('test.domain', 1000)
+    assert read_file('config_trusted_domains_output.php') == config.read()
 
-        self.assertEquals(read_file('config_trusted_domains_output.php'), config.read())
 
-    def test_trusted_http_no_port_config(self):
+def test_trusted_http_no_port_config():
 
-        config = ConfigManager(temp_config_file)
-        config.write(read_file('config_trusted_domains_no_port_input.php'))
-        config.trusted('test.domain', 80)
+    config = ConfigManager(temp_config_file)
+    config.write(read_file('config_trusted_domains_no_port_input.php'))
+    config.trusted('test.domain', 80)
 
-        self.assertEquals(read_file('config_trusted_domains_no_port_output.php'), config.read())
+    assert read_file('config_trusted_domains_no_port_output.php') == config.read()
 
-    def test_trusted_https_no_port_config(self):
 
-        config = ConfigManager(temp_config_file)
-        config.write(read_file('config_trusted_domains_no_port_input.php'))
-        config.trusted('test.domain', 443)
+def test_trusted_https_no_port_config():
 
-        self.assertEquals(read_file('config_trusted_domains_no_port_output.php'), config.read())
-    def test_trusted_config_multi_run_ho_harm(self):
+    config = ConfigManager(temp_config_file)
+    config.write(read_file('config_trusted_domains_no_port_input.php'))
+    config.trusted('test.domain', 443)
 
-        config = ConfigManager(temp_config_file)
-        config.write(read_file('config_trusted_domains_output.php'))
-        config.trusted('test.domain', 1000)
+    assert read_file('config_trusted_domains_no_port_output.php') == config.read()
 
-        self.assertEquals(read_file('config_trusted_domains_output.php'), config.read())
 
-    def test_overwrite_wewroot_config(self):
+def test_trusted_config_multi_run_ho_harm():
 
-        config = ConfigManager(temp_config_file)
-        config.write(read_file('config_overwritewebroot_input.php'))
-        config.overwrite_webroot()
+    config = ConfigManager(temp_config_file)
+    config.write(read_file('config_trusted_domains_output.php'))
+    config.trusted('test.domain', 1000)
 
-        self.assertEquals(read_file('config_overwritewebroot_output.php'), config.read())
+    assert read_file('config_trusted_domains_output.php') == config.read()
 
-    def test_overwrite_wewroot_existing_config(self):
 
-        config = ConfigManager(temp_config_file)
-        config.write(read_file('config_overwritewebroot_existing_input.php'))
-        config.overwrite_webroot()
+def test_overwrite_wewroot_config():
 
-        self.assertEquals(read_file('config_overwritewebroot_existing_output.php'), config.read())
+    config = ConfigManager(temp_config_file)
+    config.write(read_file('config_overwritewebroot_input.php'))
+    config.overwrite_webroot()
 
-    def test_overwrite_wewroot_config_multi_run_no_harm(self):
+    assert read_file('config_overwritewebroot_output.php') == config.read()
 
-        config = ConfigManager(temp_config_file)
-        config.write(read_file('config_overwritewebroot_existing_output.php'))
-        config.overwrite_webroot()
 
-        self.assertEquals(read_file('config_overwritewebroot_existing_output.php'), config.read())
+def test_overwrite_wewroot_existing_config():
+
+    config = ConfigManager(temp_config_file)
+    config.write(read_file('config_overwritewebroot_existing_input.php'))
+    config.overwrite_webroot()
+
+    read_file('config_overwritewebroot_existing_output.php') == config.read()
+
+
+def test_overwrite_wewroot_config_multi_run_no_harm():
+
+    config = ConfigManager(temp_config_file)
+    config.write(read_file('config_overwritewebroot_existing_output.php'))
+    config.overwrite_webroot()
+
+    assert read_file('config_overwritewebroot_existing_output.php') == config.read()
