898af4952da845e74e7db32bd0819e316e43dd8a
Boris Rybalkin <ribalkin@gmail.com>
2015-05-25 23:32:50 +0100

    adding nginx and php builds

---
+++ nginx/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd ${DIR}
+
+export TMPDIR=/tmp
+export TMP=/tmp
+NAME=nginx
+VERSION=1.8.0
+ROOT=/opt/syncloud-platform
+PREFIX=${ROOT}/${NAME}
+
+apt-get -y install build-essential flex bison libreadline-dev zlib1g-dev libpcre3-dev
+rm -rf nginx-${VERSION}.tar.gz*
+wget http://nginx.org/download/${NAME}-${VERSION}.tar.gz
+tar xzf ${NAME}-${VERSION}.tar.gz
+cd ${NAME}-${VERSION}
+./configure --prefix=${PREFIX}
+make
+rm -rf ${PREFIX}
+make install
+cd ..
+rm -rf ${NAME}-${VERSION}.tar.gz*
+tar czf ${NAME}-${VERSION}.tar.gz -C ${ROOT} nginx
---
+++ php/build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd ${DIR}
+
+export TMPDIR=/tmp
+export TMP=/tmp
+NAME=php
+VERSION=5.6.9
+ROOT=/opt/syncloud-owncloud
+PREFIX=${ROOT}/${NAME}
+
+apt-get -y install libxml2-dev
+rm -rf ${NAME}-${VERSION}.tar.bz2*
+wget http://php.net/get/${NAME}-${VERSION}.tar.bz2/from/this/mirror -O ${NAME}-${VERSION}.tar.bz2
+tar xjvf ${NAME}-${VERSION}.tar.bz2
+cd ${NAME}-${VERSION}
+./configure --enable-fpm --with-mysql --prefix ${PREFIX}
+make
+rm -rf ${PREFIX}
+make install
+cd ..
+tar czf ${NAME}-${VERSION}.tar.gz -C /opt ${NAME}
