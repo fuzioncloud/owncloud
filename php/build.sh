37d75b17c8c68d654e4f6c5c6422c14ca8d6f628
Boris Rybalkin <ribalkin@gmail.com>
2015-05-26 00:43:53 +0100

    enable apc support

--- php/build.sh
+++ php/build.sh
@@ -10,12 +10,20 @@ VERSION=5.6.9
 ROOT=/opt/syncloud-owncloud
 PREFIX=${ROOT}/${NAME}
 
-apt-get -y install libxml2-dev
+apt-get -y install libxml2-dev autoconf
 rm -rf ${NAME}-${VERSION}.tar.bz2*
+rm -rf ${NAME}-${VERSION}
 wget http://php.net/get/${NAME}-${VERSION}.tar.bz2/from/this/mirror -O ${NAME}-${VERSION}.tar.bz2
 tar xjf ${NAME}-${VERSION}.tar.bz2
 cd ${NAME}-${VERSION}
-./configure --enable-fpm --with-mysql --prefix ${PREFIX}
+cd ext
+wget http://pecl.php.net/get/APC-3.1.9.tgz
+tar xzf APC-3.1.9.tgz
+mv APC-3.1.9 apc
+cd ..
+rm configure
+./buildconf --force
+./configure --enable-fpm --with-mysql --enable-apc --prefix ${PREFIX} --with-config-file-path=${ROOT}/config
 make
 rm -rf ${PREFIX}
 make install
