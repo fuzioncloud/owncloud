991da03901d3e6a1b4c0f862db6643a1307e2ccf
Boris Rybalkin <ribalkin@gmail.com>
2015-05-25 23:41:34 +0100

    cleanup

--- php/build.sh
+++ php/build.sh
@@ -13,11 +13,11 @@ PREFIX=${ROOT}/${NAME}
 apt-get -y install libxml2-dev
 rm -rf ${NAME}-${VERSION}.tar.bz2*
 wget http://php.net/get/${NAME}-${VERSION}.tar.bz2/from/this/mirror -O ${NAME}-${VERSION}.tar.bz2
-tar xjvf ${NAME}-${VERSION}.tar.bz2
+tar xjf ${NAME}-${VERSION}.tar.bz2
 cd ${NAME}-${VERSION}
 ./configure --enable-fpm --with-mysql --prefix ${PREFIX}
 make
 rm -rf ${PREFIX}
 make install
 cd ..
-tar czf ${NAME}-${VERSION}.tar.gz -C /opt ${NAME}
+tar czf ${NAME}-${VERSION}.tar.gz -C ${ROOT} ${NAME}
