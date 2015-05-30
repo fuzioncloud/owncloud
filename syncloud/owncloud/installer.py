0eee9515c7d0bbd0a4d882596da0010d59e1edc3
Boris Rybalkin <ribalkin@gmail.com>
2015-05-30 11:04:48 +0100

    fixing systemd package

--- syncloud/owncloud/installer.py
+++ syncloud/owncloud/installer.py
@@ -1,39 +1,33 @@
 import os
 import shutil
 import tarfile
-from os.path import isfile, join
+from os.path import isfile
 from subprocess import check_output
+
 import MySQLdb
+
 from syncloud.sam.manager import get_sam
+from syncloud.systemd.systemctl import remove_service
+
 import wget
 
 from syncloud.owncloud.config import Config
 from syncloud.owncloud.cron import OwncloudCron
 
-
 OWNCLOUD_ARCHIVE = 'syncloud-owncloud.tar.gz'
 OWNCLOUD_ARCHIVE_TMP = '/tmp/{0}'.format(OWNCLOUD_ARCHIVE)
-SYSTEMD_NGINX_NAME = 'owncloud-nginx.service'
-SYSTEMD_PHP_FPM_NAME = 'owncloud-php-fpm.service'
 
-SYSTEMD_DIR = join('/lib', 'systemd', 'system')
+SYSTEMD_NGINX_NAME = 'owncloud-nginx'
+SYSTEMD_PHP_FPM_NAME = 'owncloud-php-fpm'
 
 
 class Installer():
     def __init__(self):
         self.config = Config()
-        self.SYSTEMD_NGINX_FILE = join(self.config.install_path(), 'config', SYSTEMD_NGINX_NAME)
-        self.SYSTEMD_PHP_FPM_FILE = join(self.config.install_path(), 'config', SYSTEMD_PHP_FPM_NAME)
         self.cron = OwncloudCron(self.config)
         self.sam = get_sam()
 
     def install(self):
-
-        con = MySQLdb.connect('localhost', 'root', 'root')
-        with con:
-            cur = con.cursor()
-            cur.execute("CREATE DATABASE IF NOT EXISTS owncloud")
-            cur.execute("GRANT ALL PRIVILEGES ON owncloud.* TO 'owncloud'@'localhost' IDENTIFIED BY 'owncloud'")
 
         if isfile(OWNCLOUD_ARCHIVE_TMP):
             os.remove(OWNCLOUD_ARCHIVE_TMP)
@@ -45,34 +39,28 @@ class Installer():
         print("extracting {0}".format(filename))
         tarfile.open(filename).extractall(self.config.root_path())
 
+        print("setup database")
+        con = MySQLdb.connect('localhost', 'root', 'root')
+        with con:
+            cur = con.cursor()
+            cur.execute("CREATE DATABASE IF NOT EXISTS owncloud")
+            cur.execute("GRANT ALL PRIVILEGES ON owncloud.* TO 'owncloud'@'localhost' IDENTIFIED BY 'owncloud'")
+
         print("setup crontab task")
         self.cron.remove()
         self.cron.create()
 
         print("setup systemd")
-        shutil.copy(self.SYSTEMD_NGINX_FILE, SYSTEMD_DIR)
-        shutil.copy(self.SYSTEMD_PHP_FPM_FILE, SYSTEMD_DIR)
-
-        check_output('systemctl enable -f {0}'.format(SYSTEMD_PHP_FPM_NAME), shell=True)
-        check_output('systemctl enable -f {0}'.format(SYSTEMD_NGINX_NAME), shell=True)
-
-        check_output('systemctl daemon-reload', shell=True)
-
-        check_output('systemctl start {0}'.format(SYSTEMD_PHP_FPM_NAME), shell=True)
-        check_output('systemctl start {0}'.format(SYSTEMD_NGINX_NAME), shell=True)
+        systemd.__add_service(self.config.install_path(), SYSTEMD_PHP_FPM_NAME)
+        self.__add_service(self.config.install_path(), SYSTEMD_NGINX_NAME)
 
     def remove(self):
 
-        check_output('systemctl stop {0}'.format(SYSTEMD_NGINX_NAME), shell=True)
-        check_output('systemctl stop {0}'.format(SYSTEMD_PHP_FPM_NAME), shell=True)
-
-        if os.path.isfile(join(SYSTEMD_DIR, self.SYSTEMD_NGINX_FILE)):
-            os.remove(join(SYSTEMD_DIR, self.SYSTEMD_NGINX_FILE))
-
-        if os.path.isfile(join(SYSTEMD_DIR, self.SYSTEMD_PHP_FPM_FILE)):
-            os.remove(join(SYSTEMD_DIR, self.SYSTEMD_PHP_FPM_FILE))
+        remove_service(SYSTEMD_NGINX_NAME)
+        __remove_service(SYSTEMD_PHP_FPM_NAME)
 
         self.cron.remove()
 
         if os.path.isdir(self.config.install_path()):
             shutil.rmtree(self.config.install_path())
+
