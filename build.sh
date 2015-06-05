9e368bc7cbfeffcb6a9e0af21aa9410432453f36
Boris Rybalkin <ribalkin@gmail.com>
2015-06-05 20:58:58 +0100

    more postgres fixes

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
 NAME=owncloud
-VERSION=8.0.3
 APP_DATA_ROOT=/opt/data/${NAME}
 USER=www-data
 
@@ -20,11 +19,24 @@ else
   echo "skipping nginx build"
 fi
 
+if [ ! -f postgresql/postgresql.tar.gz ]; then
+  ./postgresql/build.sh
+else
+  echo "skipping postgresql build"
+fi
+
+if [ ! -f owncloud/owncloud.tar.bz2 ]; then
+  ./owncloud/build.sh
+else
+  echo "skipping owncloud build"
+fi
+
 rm -rf build
 mkdir -p build/${NAME}
 
-wget https://download.owncloud.org/community/${NAME}-${VERSION}.tar.bz2 -O build/${NAME}-${VERSION}.tar.bz2
-tar xjf build/${NAME}-${VERSION}.tar.bz2 -C build/${NAME}/
+echo "packaging"
+
+tar xjf owncloud/owncloud.tar.bz2 -C build/${NAME}/
 
 cp -r bin build/${NAME}
 chown -R ${USER}. build/${NAME}/bin/
@@ -33,6 +45,7 @@ cp -r config build/${NAME}/
 
 tar xzf php/php.tar.gz -C build/${NAME}/
 tar xzf nginx/nginx.tar.gz -C build/${NAME}/
+tar xzf postgresql/postgresql.tar.gz -C build/${NAME}/
 
 mv build/${NAME}/owncloud/config build/${NAME}/owncloud/config.orig
 ln -s ${APP_DATA_ROOT}/config build/${NAME}/owncloud/config
@@ -40,5 +53,8 @@ chown -R ${USER}. build/${NAME}/owncloud/config
 
 chown -R ${USER}. build/${NAME}/owncloud/apps
 
+echo "zipping"
+
 tar cpzf ${NAME}.tar.gz -C build/ ${NAME}
 
+echo "app: ${NAME}.tar.gz"
--- syncloud/owncloud/installer.py
+++ syncloud/owncloud/installer.py
@@ -16,14 +16,13 @@ from syncloud.tools import app
 from syncloud.owncloud.config import Config
 from syncloud.owncloud.cron import OwncloudCron
 
-DB_USER = 'postgres'
 
 OWNCLOUD_ARCHIVE = 'syncloud-owncloud.tar.gz'
 OWNCLOUD_ARCHIVE_TMP = '/tmp/{0}'.format(OWNCLOUD_ARCHIVE)
 
 SYSTEMD_NGINX_NAME = 'owncloud-nginx'
 SYSTEMD_PHP_FPM_NAME = 'owncloud-php-fpm'
-SYSTEMD_POSTGRESQL = 'postgresql'
+SYSTEMD_POSTGRESQL = 'owncloud-postgresql'
 
 
 class OwncloudInstaller:
@@ -51,24 +50,10 @@ class OwncloudInstaller:
         else:
             print("app config folder already exists")
 
-        app_root = '/opt/app/owncloud/'
-        database = '/opt/data/owncloud/database'
-        if not os.path.isdir(database):
-            check_output('{0}postgresql/bin/initdb {0}'.format(app_root, database), shell=True)
-            check_output('chmod 700 {0}'.format(database), shell=True)
-            check_output('chown -R postgres. {0}'.format(database), shell=True)
-
-        with psycopg2.connect(DSN) as conn:
-            with conn.cursor() as curs:
-               curs.execute('CREATE USER owncloud WITH PASSWORD \'owncloud\'')
-               curs.execute('CREATE DATABASE owncloud TEMPLATE template0 ENCODING \'UNICODE\'')
-               curs.execute('ALTER DATABASE owncloud OWNER TO owncloud')
-               curs.execute('GRANT ALL PRIVILEGES ON DATABASE owncloud TO owncloud')
-
         try:
-            pwd.getpwnam(DB_USER)
+            pwd.getpwnam('owncloud')
         except KeyError:
-            check_output('useradd -r -s /bin/false {0}'.format(DB_USER))
+            check_output('/usr/sbin/useradd -r -s /bin/false owncloud')
 
         print("setup crontab task")
         cron.remove()
