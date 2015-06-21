5f4f8911ea9d9fa7b410576903e05b276061a839
Boris Rybalkin <ribalkin@gmail.com>
2015-06-21 16:18:34 +0100

    making ldap user an admin, removing installer account

--- syncloud/owncloud/installer.py
+++ syncloud/owncloud/installer.py
@@ -1,6 +1,8 @@
 import os
 import shutil
 from subprocess import check_output
+import uuid
+from syncloud.app import logger
 
 import psycopg2
 
@@ -8,6 +10,8 @@ from syncloud.sam.installer import Installer as SamInstaller
 from syncloud.systemd.systemctl import remove_service, add_service
 from syncloud.tools import app
 from syncloud.tools.nginx import Nginx
+import time
+from syncloud.owncloud import postgres
 from syncloud.owncloud.config import Config
 from syncloud.owncloud.cron import OwncloudCron
 from syncloud.owncloud.occ import occ
@@ -16,10 +20,12 @@ from syncloud.owncloud.setup import Setup
 SYSTEMD_NGINX_NAME = 'owncloud-nginx'
 SYSTEMD_PHP_FPM_NAME = 'owncloud-php-fpm'
 SYSTEMD_POSTGRESQL = 'owncloud-postgresql'
+INSTALL_USER = 'installer'
 
 
 class OwncloudInstaller:
     def __init__(self):
+        self.log = logger.get_logger('owncloud.installer')
         self.sam_installer = SamInstaller()
 
     def install(self, from_file=None):
@@ -32,22 +38,15 @@ class OwncloudInstaller:
         app.create_data_dir(app_data_dir, 'config', 'owncloud')
         app.create_data_dir(app_data_dir, 'data', 'owncloud')
 
-        print("setup crontab task")
-        cron = OwncloudCron(config)
-        cron.create()
-
         print("setup systemd")
         add_service(config.install_path(), SYSTEMD_POSTGRESQL)
 
-        with psycopg2.connect(database="postgres", user="owncloud", host="/tmp") as conn:
-            with conn.cursor() as curs:
-                print("creating owncloud database")
-                curs.execute('ALTER USER owncloud WITH PASSWORD \'owncloud\';')
+        postgres.execute("ALTER USER owncloud WITH PASSWORD 'owncloud';", database="postgres")
 
         add_service(config.install_path(), SYSTEMD_PHP_FPM_NAME)
         add_service(config.install_path(), SYSTEMD_NGINX_NAME)
 
-        Setup().finish('installer', 'installer')
+        Setup().finish(INSTALL_USER, unicode(uuid.uuid4().hex))
 
         occ('app:enable user_ldap')
 
@@ -68,6 +67,7 @@ class OwncloudInstaller:
         occ('ldap:set-config s01 ldapUserFilterObjectclass inetOrgPerson')
         occ('ldap:set-config s01 ldapBaseUsers ou=users,dc=syncloud,dc=org')
         occ('ldap:set-config s01 ldapUserDisplayName cn')
+        occ('ldap:set-config s01 ldapExpertUsernameAttr cn')
 
         occ('ldap:set-config s01 ldapGroupFilterObjectclass posixGroup')
         occ('ldap:set-config s01 ldapGroupDisplayName cn')
@@ -79,7 +79,18 @@ class OwncloudInstaller:
         occ('ldap:set-config s01 turnOffCertCheck 1')
         occ('ldap:set-config s01 ldapConfigurationActive 1')
 
+        cron_cmd = config.cron_cmd()
+        self.log.info("running: {0}".format(cron_cmd))
+        self.log.info(check_output(cron_cmd, shell=True))
+
+        postgres.execute("update oc_ldap_group_mapping set owncloud_name = 'admin';")
+        postgres.execute("update oc_ldap_group_members set owncloudname = 'admin';")
+
+        occ('user:delete {0}'.format(INSTALL_USER))
+
         Nginx().add_app('owncloud', config.port())
+
+        OwncloudCron(config).create()
 
     def remove(self):
 
--- syncloud/owncloud/occ.py
+++ syncloud/owncloud/occ.py
@@ -1,7 +1,11 @@
 from subprocess import check_output
 from syncloud.owncloud.config import Config
+from syncloud.app import logger
 
 
 def occ(args):
+    log = logger.get_logger('owncloud.occ')
     config = Config()
-    check_output('{0}/occ-runner {1}'.format(config.bin_dir(), args), shell=True)
+    output = check_output('{0}/occ-runner {1}'.format(config.bin_dir(), args), shell=True).strip()
+    if output:
+        log.info(output)
