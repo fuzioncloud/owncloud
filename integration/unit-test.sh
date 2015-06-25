6bf382014382b603da5cc5b2658d2d698e2ef4dd
Boris Rybalkin <ribalkin@gmail.com>
2015-06-25 19:14:56 +0100

    bring back teamcity test reporter

--- integration/unit-test.sh
+++ integration/unit-test.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
 APP_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )"  && cd .. && pwd )
-cd ${APP_DIR}/src
+cd ${APP_DIR}
 
 PYTHON_DIR=/opt/app/platform/python
 
-echo "insalling test dependencies"
 ${PYTHON_DIR}/bin/python ${PYTHON_DIR}/bin/pip2 install -U pytest
-${PYTHON_DIR}/bin/python ${PYTHON_DIR}/bin/pip2 install -r dev_requirements.txt
+${PYTHON_DIR}/bin/python ${PYTHON_DIR}/bin/pip2 install -r integration/requirements.txt
 
-echo "installing in develop mode to run unit tests"
-${PYTHON_DIR}/bin/py.test --cov syncloud test
+${PYTHON_DIR}/bin/py.test --cov src/owncloud test
