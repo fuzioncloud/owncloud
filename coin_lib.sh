d2318051a0a61d3d8141430d252c18d953c8595f
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2015-07-23 00:17:00 -0400

    Few more fixes

--- coin_lib.sh
+++ coin_lib.sh
@@ -12,4 +12,5 @@ cd lib
 coin https://pypi.python.org/packages/2.7/b/beautifulsoup4/beautifulsoup4-4.4.0-py2-none-any.whl
 coin https://pypi.python.org/packages/2.7/r/requests/requests-2.7.0-py2.py3-none-any.whl
 coin https://pypi.python.org/packages/source/m/massedit/massedit-0.67.1.zip
+coin https://pypi.python.org/packages/source/s/syncloud-app/syncloud-app-0.38.tar.gz
 cd ..
