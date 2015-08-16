0ddd6cc049f9d101278658213108018a2c05d0c0
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2015-08-16 19:27:42 -0400

    Do not need cache folder for coin packages

--- build.sh
+++ build.sh
@@ -9,9 +9,9 @@ export TMP=/tmp
 NAME=owncloud
 OWNCLOUD_VERSION=8.0.3
 
-ARCHITECTURE=$(dpkg-architecture -qDEB_HOST_GNU_CPU)
+ARCH=$(dpkg-architecture -qDEB_HOST_GNU_CPU)
 if [ ! -z "$1" ]; then
-    ARCHITECTURE=$1
+    ARCH=$1
 fi
 
 VERSION="local"
@@ -29,9 +29,9 @@ mkdir -p ${BUILD_DIR}
 
 DOWNLOAD_URL=http://build.syncloud.org:8111/guestAuth/repository/download
 
-coin --to ${BUILD_DIR} --cache_folder php_${ARCHITECTURE} raw ${DOWNLOAD_URL}/thirdparty_php_${ARCHITECTURE}/lastSuccessful/php.tar.gz
-coin --to ${BUILD_DIR} --cache_folder nginx_${ARCHITECTURE} raw ${DOWNLOAD_URL}/thirdparty_nginx_${ARCHITECTURE}/lastSuccessful/nginx.tar.gz
-coin --to ${BUILD_DIR} --cache_folder postgresql_${ARCHITECTURE} raw ${DOWNLOAD_URL}/thirdparty_postgresql_${ARCHITECTURE}/lastSuccessful/postgresql.tar.gz
+coin --to ${BUILD_DIR} raw ${DOWNLOAD_URL}/thirdparty_php_${ARCH}/lastSuccessful/php-${ARCH}.tar.gz
+coin --to ${BUILD_DIR} raw ${DOWNLOAD_URL}/thirdparty_nginx_${ARCH}/lastSuccessful/nginx-${ARCH}.tar.gz
+coin --to ${BUILD_DIR} raw ${DOWNLOAD_URL}/thirdparty_postgresql_${ARCH}/lastSuccessful/postgresql-${ARCH}.tar.gz
 coin --to ${BUILD_DIR} raw https://download.owncloud.org/community/${NAME}-${OWNCLOUD_VERSION}.tar.bz2
 
 cp -r bin ${BUILD_DIR}
@@ -45,4 +45,4 @@ echo ${NAME} >> build/${NAME}/META/app
 echo ${VERSION} >> build/${NAME}/META/version
 
 echo "zipping"
-tar cpzf ${NAME}-${VERSION}-${ARCHITECTURE}.tar.gz -C build/ ${NAME}
+tar cpzf ${NAME}-${VERSION}-${ARCH}.tar.gz -C build/ ${NAME}
