1d4285601d3949e19143716d755537f35dd95f1a
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2016-01-29 14:26:05 -0500

    Checking TeamCity build

--- coin_lib.sh
+++ coin_lib.sh
@@ -20,4 +20,5 @@ coin py https://pypi.python.org/packages/2.7/b/beautifulsoup4/beautifulsoup4-4.4
 coin py https://pypi.python.org/packages/2.7/r/requests/requests-2.7.0-py2.py3-none-any.whl
 coin py https://pypi.python.org/packages/source/m/massedit/massedit-0.67.1.zip
 coin py https://pypi.python.org/packages/source/s/syncloud-lib/syncloud-lib-2.tar.gz
+
 cd ..
