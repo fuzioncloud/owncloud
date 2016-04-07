c5f1753723fcc8b6eb01de4382050b1fba40830c
Boris Rybalkin <ribalkin@gmail.com>
2016-04-07 07:54:55 +0100

    fixing patching

--- build.sh
+++ build.sh
@@ -48,10 +48,10 @@ echo "patching filemtime fr huge file support on 32 bit php (with special compil
 cd ${BUILD_DIR}/owncloud
 patch -p0 < ${DIR}/patches/filemtime.patch
 
-LOCAL_PHP_SHA512 = $(sha512sum lib/private/files/storage/local.php | cut -d' ' -f1)
+LOCAL_PHP_SHA512=$(sha512sum lib/private/files/storage/local.php | cut -d' ' -f1)
 echo "before sha512 fix"
 grep local.php core/signature.json
-sed -i 's#"lib\\/private\\/files\\/storage\\/local\.php\": \".*\"#\"lib\\/private\\/files\\/storage\\/local.php\": \"$LOCAL_PHP_SHA512\"#g' core/signature.json
+sed -i 's#"lib\\/private\\/files\\/storage\\/local\.php\": \".*\"#\"lib\\/private\\/files\\/storage\\/local.php\": \"'$LOCAL_PHP_SHA512'\"#g' core/signature.json
 echo "after sha512 fix"
 grep local.php core/signature.json
 
