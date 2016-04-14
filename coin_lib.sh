6d4ffadec15172b51e935ea84eaec56c6e219d5b
Boris Rybalkin <ribalkin@gmail.com>
2016-04-14 21:30:10 +0100

    coin should be run outside of lib

--- coin_lib.sh
+++ coin_lib.sh
@@ -9,11 +9,7 @@ fi
 
 rm -rf lib/*
 
-cd lib
-
-coin py https://pypi.python.org/packages/2.7/b/beautifulsoup4/beautifulsoup4-4.4.0-py2-none-any.whl
-coin py https://pypi.python.org/packages/2.7/r/requests/requests-2.7.0-py2.py3-none-any.whl
-coin py https://pypi.python.org/packages/source/m/massedit/massedit-0.67.1.zip
-coin py https://pypi.python.org/packages/source/s/syncloud-lib/syncloud-lib-2.tar.gz
-
-cd ..
+coin py https://pypi.python.org/packages/2.7/b/beautifulsoup4/beautifulsoup4-4.4.0-py2-none-any.whl --to lib
+coin py https://pypi.python.org/packages/2.7/r/requests/requests-2.7.0-py2.py3-none-any.whl --to lib
+coin py https://pypi.python.org/packages/source/m/massedit/massedit-0.67.1.zip --to lib
+coin py https://pypi.python.org/packages/source/s/syncloud-lib/syncloud-lib-2.tar.gz --to lib
