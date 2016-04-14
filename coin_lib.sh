df9dec14fdc7dccfc2383a5213d9a19877deb09e
Boris Rybalkin <ribalkin@gmail.com>
2016-04-14 21:35:52 +0100

    fixing coin --to

--- coin_lib.sh
+++ coin_lib.sh
@@ -9,7 +9,7 @@ fi
 
 rm -rf lib/*
 
-coin py https://pypi.python.org/packages/2.7/b/beautifulsoup4/beautifulsoup4-4.4.0-py2-none-any.whl --to lib
-coin py https://pypi.python.org/packages/2.7/r/requests/requests-2.7.0-py2.py3-none-any.whl --to lib
-coin py https://pypi.python.org/packages/source/m/massedit/massedit-0.67.1.zip --to lib
-coin py https://pypi.python.org/packages/source/s/syncloud-lib/syncloud-lib-2.tar.gz --to lib
+coin --to lib py https://pypi.python.org/packages/2.7/b/beautifulsoup4/beautifulsoup4-4.4.0-py2-none-any.whl
+coin --to lib py https://pypi.python.org/packages/2.7/r/requests/requests-2.7.0-py2.py3-none-any.whl
+coin --to lib py https://pypi.python.org/packages/source/m/massedit/massedit-0.67.1.zip
+coin --to lib py https://pypi.python.org/packages/source/s/syncloud-lib/syncloud-lib-2.tar.gz
