4ee03ee63ddb1ca8a9ab5410bcbafbf90ace33de
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2016-04-26 20:54:55 -0400

    Removed config_path and url parameters

--- src/owncloud/config.py
+++ src/owncloud/config.py
@@ -1,23 +1,18 @@
 from ConfigParser import ConfigParser
 from os.path import join
 
-default_config_path = '/opt/app/owncloud/config'
 default_config_file = join(default_config_path, 'owncloud-ctl.cfg')
 
 
 class Config:
 
-    def __init__(self, filename=default_config_file, config_path=default_config_path):
+    def __init__(self, filename=default_config_file):
         self.parser = ConfigParser()
         self.parser.read(filename)
         self.filename = filename
-        self.config_path = config_path
 
     def port(self):
         return self.parser.getint('owncloud', 'port')
-
-    def url(self):
-        return self.parser.get('owncloud', 'url')
 
     def data_dir(self):
         return self.parser.get('owncloud', 'data_dir')
