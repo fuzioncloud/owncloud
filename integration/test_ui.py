2367ff5226122b47702b798adcfd3d29cfdf0d7e
Boris Rybalkin <ribalkin@gmail.com>
2016-12-22 18:56:39 +0000

    fix imports

--- integration/test_ui.py
+++ integration/test_ui.py
@@ -4,6 +4,7 @@ from os.path import dirname, join, exists
 
 import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support import expected_conditions as EC
