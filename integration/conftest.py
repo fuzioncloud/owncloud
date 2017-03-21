b558540110ecad2606659232b2af4fd95e4cdc11
Boris Rybalkin <ribalkin@gmail.com>
2017-03-21 08:54:13 +0000

    simplify app installation for tests

--- integration/conftest.py
+++ integration/conftest.py
@@ -9,8 +9,7 @@ def pytest_addoption(parser):
     parser.addoption("--password", action="store")
     parser.addoption("--domain", action="store")
     parser.addoption("--release", action="store")
-    parser.addoption("--app-version", action="store")
-    parser.addoption("--arch", action="store")
+    parser.addoption("--app_archive_path", action="store")
 
 
 @pytest.fixture(scope="session")
@@ -20,8 +19,7 @@ def auth(request):
            config.getoption("--password"), \
            config.getoption("--domain"), \
            config.getoption("--release"), \
-           config.getoption("--app-version"), \
-           config.getoption("--arch")
+           config.getoption("--app_archive_path")
 
 
 @pytest.fixture(scope='module')
@@ -29,4 +27,8 @@ def user_domain(auth):
     email, password, domain, release, version, arch = auth
     return 'owncloud.{0}.{1}'.format(domain, SYNCLOUD_INFO)
 
+pytest.fixture(scope='module')
+def app_archive_path(auth):
+    _, _, _, _, app_archive = auth
+    return app_archive
 
