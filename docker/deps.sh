bf585563803574d8e0e41107c1d38f2decbdf9c0
Boris Rybalkin <ribalkin@gmail.com>
2017-08-10 08:41:13 +0100

    fix phantomjs

--- docker/deps.sh
+++ docker/deps.sh
@@ -8,7 +8,7 @@ pip install -U pytest
 pip install coin
 ARCH=$(dpkg-architecture -q DEB_HOST_ARCH)
 if [ $ARCH == "amd64" ]; then
-  wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+  wget http://artifact.syncloud.org/3rdparty/phantomjs-2.1.1-linux-x86_64.tar.bz2
   tar xjvf phantomjs-2.1.1-linux-x86_64.tar.bz2
   cp ./phantomjs-2.1.1-linux-x86_64/bin/phantomjs /usr/bin
 else
