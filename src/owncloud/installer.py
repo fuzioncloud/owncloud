f59c626571c8f9bd41add811922a9ce264fe2d82
Boris Rybalkin <ribalkin@gmail.com>
2017-08-12 23:51:09 +0100

    fix postgres port

--- src/owncloud/installer.py
+++ src/owncloud/installer.py
@@ -24,6 +24,7 @@ DB_NAME = 'owncloud'
 DB_USER = 'owncloud'
 DB_PASSWORD = 'owncloud'
 PSQL_PATH = 'postgresql/bin/psql'
+PSQL_PORT = 5435
 OCC_RUNNER_PATH = 'bin/occ-runner'
 OC_CONFIG_PATH = 'bin/owncloud-config'
 OWNCLOUD_LOG_PATH = 'log/owncloud.log'
@@ -67,7 +68,8 @@ class OwncloudInstaller:
         variables = {
             'app_dir': self.app.get_install_dir(),
             'app_data_dir': app_data_dir,
-            'web_port': WEB_PORT
+            'web_port': WEB_PORT,
+            'db_psql_port': PSQL_PORT,
         }
         gen.generate_files(templates_path, config_path, variables)
         fs.chownpath(self.app.get_install_dir(), USER_NAME, recursive=True)
